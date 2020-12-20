2e0890dac229a57baa8d2b8d2ce99db571fac670
skskdmsdl <skskdmsdl@gmail.com>
2020-12-20 14:56:43 +0900

    function으로 빼기

--- mian.py
+++ mian.py
@@ -1,16 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from naver import extract_naver_pages, extract_naver_news
 
-naver_result = requests.get("https://search.naver.com/search.naver?where=news&query=삼성전자&sm=tab_opt&sort=1&photo=0&field=0&reporter_article=&pd=6&ds=&de=&docid=&nso=so%3Add%2Cp%3A6m%2Ca%3Aall&mynews=0&refresh_start=0&related=0")
+last_naver_pages = extract_naver_pages()
 
-naver_soup = BeautifulSoup(naver_result.text, "html.parser")
-
-# find로 찾은 결과를 sc_page_inner 변수에 넣고 리스트를 만들어 pages 변수에 넣어주기
-sc_page_inner = naver_soup.find("div", {"class": "sc_page_inner"})  #페이징이 있는 클래스 찾기
-
-pages = sc_page_inner.find_all('a')  #페이지를 알기 위해 모든 a태그 찾기 (여기서 pages는 list임)
-
-
-
-print(pages)
-
+naver_news = extract_naver_news(last_naver_pages)
---
+++ naver.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://search.naver.com/search.naver?&where=news&query=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sm=tab_pge&sort=1&photo=0&field=0&reporter_article=&pd=6&ds=2020.06.23&de=2020.12.20&docid=&nso=so:dd,p:6m,a:all&mynews=0"
+
+def extract_naver_pages():
+    result = requests.get(URL)
+
+    soup = BeautifulSoup(result.text, "html.parser")
+
+    # find로 찾은 결과를 sc_page_inner 변수에 넣고 리스트를 만들어 pages 변수에 넣어주기
+    sc_page_inner = soup.find("div", {"class": "sc_page_inner"})  #페이징이 있는 클래스 찾기
+
+    links = sc_page_inner.find_all('a')  #페이지를 알기 위해 모든 a태그 찾기 (여기서 pages는 list임)
+    pages = []
+
+    for link in links:
+        pages.append(int(link.string))
+
+    max_page = pages[-1]
+    return max_page
+
+def extract_naver_news(last_page):
+    news = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&start={page*10}&refresh_start=0")
+        print(result.status_code)
+    return news
